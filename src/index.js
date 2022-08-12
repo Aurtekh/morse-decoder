246d9de4ff293eed4fe0d430d2c6da540d541536
Daniil Ermolovich <ermolovichd007@gmail.com>
2022-08-12 23:18:53 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrMorz = [];
+    let decode = [];
+    let arrBin = [];
+    let result = '';
+    let i = 0;
+    for (let i = 0; i < expr.length; i += 10) {
+      arrBin.push(expr.slice(i, i + 10));
+    }
+     for (let i = 0; i < arrBin.length; i++) {
+       for (let j = 0; j < arrBin[i].length; j +=2) {
+          if (arrBin[i][j] === '1' && arrBin[i][j+1] === '0') {
+        arrMorz += '.'
+      }
+          if (arrBin[i][j] === '1' && arrBin[i][j+1] === '1') {
+        arrMorz += '-'
+      }
+         if (arrBin[i][j] === '*') {
+           arrMorz += ' ';
+  
+         }
+       }
+       decode.push(arrMorz);
+       arrMorz = [];
+    }
+    for (let i = 0; i < decode.length; i++) {
+      if (decode[i] === '     ') {
+        result += ' ';
+      } else result += MORSE_TABLE[decode[i]];
+    }
+    return result
 }
 
 module.exports = {
